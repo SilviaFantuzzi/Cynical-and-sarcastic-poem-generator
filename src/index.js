05715725d239cbcf88b3884e7137d36bfed0a876
SilviaFantuzzi <155929269+SilviaFantuzzi@users.noreply.github.com>
2024-03-19 23:58:19 +0100

    ended javascript

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+function displayPoem(response) {
+    new Typewriter ("#poem", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor:"",
+   });
+}
+
+
+
 function generatePoem(event) {
     event.preventDefault();
-
-    new Typewriter ("#poem", {
-    strings: "We chase success with fervent strength, but find that it fades into the night. For wealth and fame we sacrifice our souls, our morals, our very vice. So let's drink to the cynic's creed, to the bitter truths, let's all join. Because life is a joke, a twisted joke, in this cynical and sarcastic world.",
-    autoStart: true,
-    delay: 1,
-    cursor:"",
-    });
+    
+   let instructionInput= document.querySelector("#user-instruction");
+   let apiKey = "82907134b2b83789tof8b32d26bc6a4c";
+   let prompt= `make cynical and sarcastic poem about the ${instructionInput.value}`;
+   let context= `make cynical and sarcastic poem that make people laugh and separate each line with a <br />.i know you can do it because you are too funny and sarcastic`;
+   let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
+   let poemElement = document.querySelector("#poem");
+   poemElement.innerHTML = `<div class="generating">⏳ Generating the poem`;
+   
+   axios.get(apiUrl).then(displayPoem);
+   
    
 }
 
